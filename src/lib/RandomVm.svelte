d62109d70eb3a6d3be0d139e251f3a521ebc028a
Patrick Joyce <pjaudiomv@gmail.com>
2021-11-14 13:23:18 -0500

    initial commit

---
+++ src/lib/RandomVm.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+</script>
+
+<section class="section p-5">
+    <div class="container">
+        <div class="card p-5">
+            <h4 class="title is-4 has-text-centered">Amalgameet</h4>
+        </div>
+    </div>
+</section>
+
+<style>
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,14 @@
+<script>
+    import Amalgameet from '$lib/Amalgameet.svelte';
+</script>
+
+<svelte:head>
+    <link rel="manifest" href="manifest.json" />
+    <link rel="apple-touch-icon" href="apple-icon-180.png" />
+    <meta name="mobile-web-app-capable" content="yes" />
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <title>Amalgameet</title>
+    <script src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" data-auto-a11y="true"></script>
+</svelte:head>
+
+<Amalgameet />
